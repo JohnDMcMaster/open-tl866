54b22f00b5b15ae4e45acf0be9bc1ae568902b5f
William D. Jones <thor0505@comcast.net>
2018-04-09 06:31:15 -0400

    Led is blinking.

--- firmware/main.c
+++ firmware/main.c
@@ -17,5 +17,21 @@ void interrupt low_priority intlo(void)
 
 int main(void)
 {
+    PORTC = 0x00;
+    TRISCbits.RC0 = 0;
+    PORTCbits.RC0 = 0;
+    
+    int blink = 0;
+    
+    while(1)
+    {
+        for(long i = 0; i < 100000; i++)
+        {
+        }
+        
+        blink ^= 0x01;
+        PORTCbits.RC0 = blink;
+    }
+    
     return 0;
 }
