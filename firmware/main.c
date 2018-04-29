d875968c3f791b97b57d01b0ce24893a9f499edf
John McMaster <johndmcmaster@gmail.com>
2018-04-29 10:19:53 -0700

    Mixed tab/spaces => spaces
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- firmware/main.c
+++ firmware/main.c
@@ -37,24 +37,24 @@ int echo = 0;
 
 static void send_string_sync(uint8_t endpoint, const char *str)
 {
-	char *in_buf = (char*) usb_get_in_buffer(endpoint);
+    char *in_buf = (char*) usb_get_in_buffer(endpoint);
 
-	while (usb_in_endpoint_busy(endpoint))
-		;
+    while (usb_in_endpoint_busy(endpoint))
+        ;
 
-	strcpy(in_buf, str);
-	/* Hack: Get the length from strlen(). This is inefficient, but it's
-	 * just a demo. strlen()'s return excludes the terminating NULL. */
-	usb_send_in_buffer(endpoint, strlen(in_buf));
+    strcpy(in_buf, str);
+    /* Hack: Get the length from strlen(). This is inefficient, but it's
+     * just a demo. strlen()'s return excludes the terminating NULL. */
+    usb_send_in_buffer(endpoint, strlen(in_buf));
 }
 
 static inline void init(void) {
         unsigned int pll_startup = 600;
-	OSCTUNEbits.PLLEN = 1;
-	while (pll_startup--);
+    OSCTUNEbits.PLLEN = 1;
+    while (pll_startup--);
     
     INTCONbits.PEIE = 1;
-	INTCONbits.GIE = 1;
+    INTCONbits.GIE = 1;
     
     WDTCONbits.ADSHR = 1;
     ANCON0 |= 0x9F; // Disable analog functionality on Ports A, F, and H.
@@ -118,7 +118,7 @@ static inline void init(void) {
     OE_VPP = 0;
     OE_VDD = 0;
     
-	usb_init();
+    usb_init();
 
     PORTCbits.RC0 = 1;
 }
@@ -229,28 +229,28 @@ int main(void)
 {
     init();
 
-	uint8_t char_to_send = 'A';
-	bool send = true;
-	bool loopback = false;
+    uint8_t char_to_send = 'A';
+    bool send = true;
+    bool loopback = false;
 
     unsigned char cmd_buf[64];
     int cmd_ptr = 0;
 
-	while (1) {
-		/* Handle data received from the host */
-		if (usb_is_configured() &&
-		    !usb_out_endpoint_halted(2) &&
-		    usb_out_endpoint_has_data(2)) {
+    while (1) {
+        /* Handle data received from the host */
+        if (usb_is_configured() &&
+            !usb_out_endpoint_halted(2) &&
+            usb_out_endpoint_has_data(2)) {
             
-			const unsigned char *out_buf;
-			size_t out_buf_len;
+            const unsigned char *out_buf;
+            size_t out_buf_len;
             int newline_found = 0;
             parse_result_t res;
 
-			/* Check for an empty transaction. */
-			out_buf_len = usb_get_out_buffer(2, &out_buf);
-			if (out_buf_len <= 0)
-				goto empty;
+            /* Check for an empty transaction. */
+            out_buf_len = usb_get_out_buffer(2, &out_buf);
+            if (out_buf_len <= 0)
+                goto empty;
             
             /* If copying would overflow, discard whole command and error. */
             if(cmd_ptr + out_buf_len > 63)
@@ -288,11 +288,11 @@ int main(void)
             cmd_ptr = 0;
             
 empty:
-			usb_arm_out_endpoint(2);
-		}
-	}
+            usb_arm_out_endpoint(2);
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 /* Callbacks. These function names are set in usb_config.h. */
@@ -303,7 +303,7 @@ void app_set_configuration_callback(uint8_t configuration)
 
 uint16_t app_get_device_status_callback()
 {
-	return 0x0000;
+    return 0x0000;
 }
 
 void app_endpoint_halt_callback(uint8_t endpoint, bool halted)
@@ -313,12 +313,12 @@ void app_endpoint_halt_callback(uint8_t endpoint, bool halted)
 
 int8_t app_set_interface_callback(uint8_t interface, uint8_t alt_setting)
 {
-	return 0;
+    return 0;
 }
 
 int8_t app_get_interface_callback(uint8_t interface)
 {
-	return 0;
+    return 0;
 }
 
 void app_out_transaction_callback(uint8_t endpoint)
@@ -333,20 +333,20 @@ void app_in_transaction_complete_callback(uint8_t endpoint)
 
 int8_t app_unknown_setup_request_callback(const struct setup_packet *setup)
 {
-	/* To use the CDC device class, have a handler for unknown setup
-	 * requests and call process_cdc_setup_request() (as shown here),
-	 * which will check if the setup request is CDC-related, and will
-	 * call the CDC application callbacks defined in usb_cdc.h. For
-	 * composite devices containing other device classes, make sure
-	 * MULTI_CLASS_DEVICE is defined in usb_config.h and call all
-	 * appropriate device class setup request functions here.
-	 */
-	return process_cdc_setup_request(setup);
+    /* To use the CDC device class, have a handler for unknown setup
+     * requests and call process_cdc_setup_request() (as shown here),
+     * which will check if the setup request is CDC-related, and will
+     * call the CDC application callbacks defined in usb_cdc.h. For
+     * composite devices containing other device classes, make sure
+     * MULTI_CLASS_DEVICE is defined in usb_config.h and call all
+     * appropriate device class setup request functions here.
+     */
+    return process_cdc_setup_request(setup);
 }
 
 int16_t app_unknown_get_descriptor_callback(const struct setup_packet *pkt, const void **descriptor)
 {
-	return -1;
+    return -1;
 }
 
 void app_start_of_frame_callback(void)
@@ -363,7 +363,7 @@ void app_usb_reset_callback(void)
 
 int8_t app_send_encapsulated_command(uint8_t interface, uint16_t length)
 {
-	return -1;
+    return -1;
 }
 
 int16_t app_get_encapsulated_response(uint8_t interface,
@@ -371,66 +371,66 @@ int16_t app_get_encapsulated_response(uint8_t interface,
                                       usb_ep0_data_stage_callback *callback,
                                       void **context)
 {
-	return -1;
+    return -1;
 }
 
 int8_t app_set_comm_feature_callback(uint8_t interface,
                                      bool idle_setting,
                                      bool data_multiplexed_state)
 {
-	return -1;
+    return -1;
 }
 
 int8_t app_clear_comm_feature_callback(uint8_t interface,
                                        bool idle_setting,
                                        bool data_multiplexed_state)
 {
-	return -1;
+    return -1;
 }
 
 int8_t app_get_comm_feature_callback(uint8_t interface,
                                      bool *idle_setting,
                                      bool *data_multiplexed_state)
 {
-	return -1;
+    return -1;
 }
 
 static struct cdc_line_coding line_coding =
 {
-	115200,
-	CDC_CHAR_FORMAT_1_STOP_BIT,
-	CDC_PARITY_NONE,
-	8,
+    115200,
+    CDC_CHAR_FORMAT_1_STOP_BIT,
+    CDC_PARITY_NONE,
+    8,
 };
 
 int8_t app_set_line_coding_callback(uint8_t interface,
                                     const struct cdc_line_coding *coding)
 {
-	line_coding = *coding;
-	return 0;
+    line_coding = *coding;
+    return 0;
 }
 
 int8_t app_get_line_coding_callback(uint8_t interface,
                                     struct cdc_line_coding *coding)
 {
-	/* This is where baud rate, data, stop, and parity bits are set. */
-	*coding = line_coding;
-	return 0;
+    /* This is where baud rate, data, stop, and parity bits are set. */
+    *coding = line_coding;
+    return 0;
 }
 
 int8_t app_set_control_line_state_callback(uint8_t interface,
                                            bool dtr, bool dts)
 {
-	return 0;
+    return 0;
 }
 
 int8_t app_send_break_callback(uint8_t interface, uint16_t duration)
 {
-	return 0;
+    return 0;
 }
 
 
 void interrupt high_priority isr()
 {
-	usb_service();
+    usb_service();
 }
