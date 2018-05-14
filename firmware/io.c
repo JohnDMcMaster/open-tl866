29fa0ca610ca25adbc4b01abbda2cb8ba9e3def6
William D. Jones <thor0505@comcast.net>
2018-05-14 16:50:43 -0400

    Don't overwrite non-ZIF I/O.

--- firmware/io.c
+++ firmware/io.c
@@ -7,6 +7,7 @@ static void ports_to_zif_pins(port_bits_t port, zif_bits_t zif);
 static void zif_pins_to_ports(zif_bits_t zif, port_bits_t port);
 static void port_read_all(port_bits_t);
 static void port_write_all(port_bits_t);
+static void dir_read_all(port_bits_t p_bits);
 static void dir_write_all(port_bits_t p_bits);
 
 #define OFFS_A 0
@@ -31,7 +32,7 @@ const port_info_t zif2port[40] = {
     {OFFS_J, 6},
     {OFFS_C, 6},
     {OFFS_C, 7},
-    
+
     {OFFS_J, 4},
     {OFFS_J, 5},
     {OFFS_G, 3},
@@ -40,7 +41,7 @@ const port_info_t zif2port[40] = {
     {OFFS_D, 1},
     {OFFS_D, 2},
     {OFFS_G, 1},
-    
+
     {OFFS_E, 0},
     {OFFS_E, 7},
     {OFFS_E, 2},
@@ -49,7 +50,7 @@ const port_info_t zif2port[40] = {
     {OFFS_E, 5},
     {OFFS_E, 6},
     {OFFS_E, 1},
-    
+
     {OFFS_D, 3},
     {OFFS_D, 4},
     {OFFS_D, 5},
@@ -58,7 +59,7 @@ const port_info_t zif2port[40] = {
     {OFFS_G, 0},
     {OFFS_J, 0},
     {OFFS_J, 1},
-    
+
     {OFFS_J, 2},
     {OFFS_J, 3},
     {OFFS_B, 2},
@@ -91,7 +92,7 @@ const latch_info_t zif2vdd[40] = {
     {0, -1},
     {0, -1},
     {0, -1},
-    
+
     {0, -1},
     {0, -1},
     {0, -1},
@@ -128,47 +129,47 @@ const latch_info_t zif2vpp[40] = {
     {0, 3},
     {1, 2},
     {1, 3},
-    
+
     {0, -1},
     {0, -1},
     {0, -1},
     {0, -1},
-    
+
     {1, 5},
     {1, 4},
     {0, -1},
     {0, -1},
-    
-    {0, -1},   
-    {0, -1},
-    {0, -1},
-    {0, -1},
-    
+
     {0, -1},
     {0, -1},
     {0, -1},
     {0, -1},
-    
+
     {0, -1},
     {0, -1},
     {0, -1},
     {0, -1},
-    
+
     {0, -1},
     {0, -1},
     {0, -1},
     {0, -1},
-    
+
+    {0, -1},
+    {0, -1},
+    {0, -1},
+    {0, -1},
+
     {0, -1},
     {0, 0},
     {1, 0},
     {0, 7},
-    
+
     {1, 6},
     {1, 1},
     {0, -1},
     {0, 1},
-    
+
     {1, 7},
     {0, 6},
     {0, 5},
@@ -233,9 +234,10 @@ static unsigned char latch_mirror[8]; /* Read mirror of the current latch state.
 void dir_write(zif_bits_t zif_val)
 {
     port_bits_t port_val = {0};
-    
+
+    dir_read_all(port_val);
     zif_pins_to_ports(zif_val, port_val);
-    
+
     dir_write_all(port_val);
 }
 
@@ -243,16 +245,17 @@ void dir_write(zif_bits_t zif_val)
 void zif_write(zif_bits_t zif_val)
 {
     port_bits_t port_val = {0};
-    
+
+    port_read_all(port_val);
     zif_pins_to_ports(zif_val, port_val);
-    
+
     port_write_all(port_val);
 }
 
 void zif_read(zif_bits_t zif_val)
 {
     port_bits_t port_val = {0};
-    
+
     port_read_all(port_val);
     ports_to_zif_pins(port_val, zif_val);
 }
@@ -265,12 +268,14 @@ static void zif_pins_to_ports(zif_bits_t zif, port_bits_t port)
         shifts, but XC8 is broken in free mode. */
         unsigned char set_of_8 = (pin_no >> 3);
         unsigned char bit_offset = 1 << (pin_no & 0x07);
-        
+
         unsigned char pin_val = (zif[set_of_8] & bit_offset) ? 1 : 0;
-        
-        port_info_t curr = zif2port[pin_no];      
+
+        port_info_t curr = zif2port[pin_no];
+        port[curr.bank] &= ~(1 << curr.offset); /* ZIF pins are shared w/
+        control pins. Don't accidentally erase control pin values. */
         port[curr.bank] |= (pin_val << curr.offset);
-    } 
+    }
 }
 
 
@@ -283,9 +288,9 @@ static void ports_to_zif_pins(port_bits_t port, zif_bits_t zif)
         unsigned char set_of_8 = (pin_no >> 3);
         unsigned char bit_offset = (pin_no & 0x07);
         unsigned char port_offset = (1 << curr.offset);
-        
+
         unsigned char pin_val = (port[curr.bank] & port_offset) ? 1 : 0;
-   
+
         zif[set_of_8] |= (pin_val << bit_offset);
     }
 }
@@ -318,6 +323,19 @@ static void port_write_all(port_bits_t p_bits)
     PORTJ = p_bits[8];
 }
 
+static void dir_read_all(port_bits_t p_bits)
+{
+    p_bits[0] = TRISA;
+    p_bits[1] = TRISB;
+    p_bits[2] = TRISC;
+    p_bits[3] = TRISD;
+    p_bits[4] = TRISE;
+    p_bits[5] = TRISF;
+    p_bits[6] = TRISG;
+    p_bits[7] = TRISH;
+    p_bits[8] = TRISJ;
+}
+
 static void dir_write_all(port_bits_t p_bits)
 {
     TRISA = p_bits[0];
@@ -335,7 +353,7 @@ static void dir_write_all(port_bits_t p_bits)
 /* Read mask of I/O pin dir (TRIS) into array in ZIF order. */
 void zifdir_mask(unsigned char (*)[5])
 {
-    
+
 }
 
 
@@ -343,10 +361,10 @@ void zifdir_mask(unsigned char (*)[5])
 void write_latch(int latch_no, unsigned char val)
 {
     write_shreg(val);
-    
+
     //74hc373's setup time: 15ns hold time: 5ns
     //LE == 0 to preserve output, 1 is "transparent mode".
-    
+
     switch(latch_no)
     {
         case 0:
@@ -399,7 +417,7 @@ void write_latch(int latch_no, unsigned char val)
             break;
         default:
             break;
-        
+
     }
 }
 
@@ -409,14 +427,14 @@ void write_shreg(unsigned char in)
     for(int i = 0; i < 8; i++)
     {
         unsigned char curr_bit = (in & 0x80) ? 1 : 0;
-        
+
         SR_DAT = curr_bit;
         // 74hc164's data setup time: 20-100ns Hold time: 5ns
         __delay_us(1);
         SR_CLK = 1;
         __delay_us(1);
         SR_CLK = 0;
-        
+
         in = in << 1;
     }
 }
@@ -424,7 +442,7 @@ void write_shreg(unsigned char in)
 /* Read mirror of one of the 8 pin driver latches */
 /* unsigned char read_latch(int)
 {
-    
+
 } */
 
 void vpp_en(void)
@@ -451,7 +469,7 @@ void vdd_dis(void)
 void set_vpp(zif_bits_t zif)
 {
     unsigned char latch_vpp_masks[2] = { 0 };
-    
+
     for(unsigned int pin_no = 0; pin_no < (sizeof(zif2vpp)/sizeof(latch_info_t)); pin_no++)
     {
         latch_info_t curr = zif2vpp[pin_no];
@@ -466,13 +484,13 @@ void set_vpp(zif_bits_t zif)
         {
             unsigned char set_of_8 = (pin_no >> 3);
             unsigned char bit_offset = 1 << (pin_no & 0x07);
-        
+
             unsigned char mask = (zif[set_of_8] & bit_offset) ? 1 : 0;
-      
+
             latch_vpp_masks[curr.number] |= (mask << (unsigned char) curr.offset);
         }
     }
-    
+
     write_latch(0, latch_vpp_masks[0]);
     write_latch(1, latch_vpp_masks[1]);
 }
@@ -480,7 +498,7 @@ void set_vpp(zif_bits_t zif)
 void set_vdd(zif_bits_t zif)
 {
     unsigned char latch_vdd_masks[3] = { 0 };
-    
+
     for(unsigned int pin_no = 0; pin_no < (sizeof(zif2vdd)/sizeof(latch_info_t)); pin_no++)
     {
         latch_info_t curr = zif2vdd[pin_no];
@@ -493,10 +511,10 @@ void set_vdd(zif_bits_t zif)
         {
             unsigned char set_of_8 = (pin_no >> 3);
             unsigned char bit_offset = 1 << (pin_no & 0x07);
-        
+
             // Drivers are PNPs, so a logic 0 enables Vdd line for I/O pin.
             unsigned char mask = (zif[set_of_8] & bit_offset) ? 1 : 0;
-      
+
             latch_vdd_masks[curr.number - 2] |= (mask << (unsigned char) curr.offset);
         }
     }
@@ -509,7 +527,7 @@ void set_vdd(zif_bits_t zif)
 void set_gnd(zif_bits_t zif)
 {
     unsigned char latch_gnd_masks[3] = { 0 };
-    
+
     for(unsigned int pin_no = 0; pin_no < (sizeof(zif2gnd)/sizeof(latch_info_t)); pin_no++)
     {
         latch_info_t curr = zif2gnd[pin_no];
@@ -522,14 +540,14 @@ void set_gnd(zif_bits_t zif)
         {
             unsigned char set_of_8 = (pin_no >> 3);
             unsigned char bit_offset = 1 << (pin_no & 0x07);
-        
+
             // Drivers are PNPs, so a logic 0 enables Vdd line for I/O pin.
             unsigned char mask = (zif[set_of_8] & bit_offset) ? 1 : 0;
-      
+
             latch_gnd_masks[curr.number - 5] |= (mask << (unsigned char) curr.offset);
         }
     }
-    
+
     write_latch(5, latch_gnd_masks[0]);
     write_latch(6, latch_gnd_masks[1]);
     write_latch(7, latch_gnd_masks[2]);
@@ -538,21 +556,21 @@ void set_gnd(zif_bits_t zif)
 void vpp_val(unsigned char setting)
 {
     setting &= 0x07;
-    
+
     VID_10 = (setting & 0x01) ? 1 : 0;
     VID_11 = (setting & 0x02) ? 1 : 0;
     VID_12 = (setting & 0x04) ? 1 : 0;
-    
+
     __delay_ms(2);
 }
 
 void vdd_val(unsigned char setting)
 {
     setting &= 0x07;
-    
+
     VID_00 = (setting & 0x01) ? 1 : 0;
     VID_01 = (setting & 0x02) ? 1 : 0;
     VID_02 = (setting & 0x04) ? 1 : 0;
-    
+
     __delay_ms(2);
 }
