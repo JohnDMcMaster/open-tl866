bab5551157d02390ecd28982a01646b7d1eba5b8
Peter Bortas <bortas@gmail.com>
2018-08-20 22:12:31 +0200

    Fix pytl866 module install

--- pytl866/setup.py
+++ pytl866/setup.py
@@ -10,7 +10,7 @@ setup(
     author="William D. Jones",
     author_email="thor0505@comcast.net",
     license="BSD",
-    packages=["pytl866"],
+    packages=["pytl866", "pytl866/bootloader"],
     install_requires=[
         "intelhex",
         "pyserial",
