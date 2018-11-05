fcaf70553100cbf35c272668ac99c4f3b8171a25
John McMaster <johndmcmaster@gmail.com>
2018-11-04 21:10:17 -0800

    bitbang: rejigger options
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- firmware/modes/bitbang/main.c
+++ firmware/modes/bitbang/main.c
@@ -16,24 +16,27 @@ static inline void print_help(void)
     com_println("open-tl866 (bitbang)");
     com_println("VPP");
     com_println("E val      VPP: enable and/or disable (VPP_DISABLE/VPP_ENABLE)");
-    com_println("P val      VPP: set voltage enum (VPP_SET)");
+    com_println("V val      VPP: set voltage enum (VPP_SET)");
     com_println("p val      VPP: set active pins (VPP_WRITE)");
+    //com_println("P val      VPP: get active pins (VPP_READ)");
     com_println("VDD");
     com_println("e val      VDD: enable and/or disable (VDD_DISABLE/VDD_ENABLE)");
-    com_println("D val      VDD: set voltage enum (VDD_SET)");
+    com_println("v val      VDD: set voltage enum (VDD_SET)");
     com_println("d val      VDD: set active pins (VDD_WRITE)");
+    //com_println("D val      VDD: get active pins (VDD_READ)");
     com_println("GND");
     com_println("g val      GND: set active pins (GND_WRITE)");
+    //com_println("G val      GND: get active pins (GND_READ)");
     com_println("I/O");
-    com_println("t val      write ZIF tristate setting (ZIF_DIR)");
-    com_println("T          read ZIF tristate setting (ZIF_DIR_READ)");
-    com_println("z val      write ZIF pins (ZIF_WRITE)");
-    com_println("Z          read ZIF pins (ZIF_READ)");
+    com_println("t val      I/O: set ZIF tristate setting (ZIF_DIR)");
+    com_println("T          I/O: get ZIF tristate setting (ZIF_DIR_READ)");
+    com_println("z val      I/O: set ZIF pins (ZIF_WRITE)");
+    com_println("Z          I/O: get ZIF pins (ZIF_READ)");
     com_println("Misc");
     com_println("l val      LED on/off (LED_ON/LED_OFF)");
-    com_println("L          LED read state (LED_QUERY)");
-    com_println("m val      pullup/pulldown (MYSTERY_ON/MYSTERY_OFF}");
-    com_println("b          reset to bootloader (RESET_BOOTLOADER)");
+    com_println("m z val    Set pullup/pulldown (MYSTERY_ON/MYSTERY_OFF}");
+    com_println("s          Print misc status");
+    com_println("b          Reset to bootloader (RESET_BOOTLOADER)");
 }
 
 static inline void eval_command(char *cmd)
@@ -49,6 +52,7 @@ static inline void eval_command(char *cmd)
     VPP
     */
 
+    //VPP enable/disable
     case 'E':
         if (arg_i()) {
             if (last_i) {
@@ -59,23 +63,37 @@ static inline void eval_command(char *cmd)
         }
         break;
 
-    case 'P':
+    //VPP voltage value
+    case 'V':
         if (arg_i()) {
             vpp_val(last_i);
         }
         break;
 
+    //Set VPP pins
     case 'p':
         if (arg_zif()) {
             set_vpp(last_zif);
         }
         break;
 
+    /*
+    //Get VPP pins
+    case 'p':
+    {
+        zif_bits_t zif = { 0x00 };
+        get_vpp(zif);
+        print_zif_bits("Result", zif);
+        break;
+    }
+    */
+
 
     /*
     VDD
     */
 
+    //VDD enable/disable
     case 'e':
         if (arg_i()) {
             if (last_i) {
@@ -86,48 +104,66 @@ static inline void eval_command(char *cmd)
         }
         break;
 
-    case 'D':
+    //VDD voltage value
+    case 'v':
         if (arg_i()) {
             vdd_val(last_i);
         }
         break;
 
+    //Set VDD pins
     case 'd':
         if (arg_zif()) {
             set_vdd(last_zif);
         }
         break;
 
+    /*
+    //Get VDD pins
+    case 'D':
+    {
+        zif_bits_t zif = { 0x00 };
+        get_vdd(zif);
+        print_zif_bits("Result", zif);
+        break;
+    }
+    */
 
     /*
     GND
     */
 
+    //Set GND pins
     case 'G':
         if (arg_zif()) {
             set_gnd(last_zif);
         }
         break;
 
+    /*
+    //Get GND pins
+    case 'G':
+    {
+        zif_bits_t zif = { 0x00 };
+        get_gnd(zif);
+        print_zif_bits("Result", zif);
+        break;
+    }
+    */
+
 
     /*
     I/O
     */
 
+    //Set I/O tristate
     case 't':
         if (arg_zif()) {
             dir_write(last_zif);
         }
         break;
 
-    case 'Z':
-    {
-        zif_bits_t zif = { 0x00 };
-        zif_read(zif);
-        print_zif_bits("Result", zif);
-        break;
-    }
-
+    //Get I/O tristate
     case 'T':
     {
         zif_bits_t zif = { 0x00 };
@@ -136,17 +172,27 @@ static inline void eval_command(char *cmd)
         break;
     }
 
+    //Write ZIF socket
     case 'z':
         if (arg_zif()) {
             zif_write(last_zif);
         }
         break;
 
+    //Read ZIF socket
+    case 'Z':
+    {
+        zif_bits_t zif = { 0x00 };
+        zif_read(zif);
+        print_zif_bits("Result", zif);
+        break;
+    }
 
     /*
     Misc
     */
 
+    //LED on/off
     case 'l':
     {
         if (arg_bit()) {
@@ -155,23 +201,35 @@ static inline void eval_command(char *cmd)
         break;
     }
 
-    case 'L':
-        printf("Result: %d\r\n", LED);
+    //Pullup/pulldown on/off
+    case 'm': {
+        if (arg_bit()) {
+            int tristate = last_bit;
+            if (arg_bit()) {
+                pupd(tristate, last_bit);
+            }
+        }
+        break;
+    }
+
+    //Misc status
+    case 's':
+        printf("Result nVPP_EN:%u nVDD_EN:%u LED:%u PUPD:Z%uV%u\r\n",
+                OE_VPP, OE_VDD, LED, PUPD_TRIS, PUPD_PORT);
         break;
 
-    case 'm':
-        PUPD_PORT = arg_bit();
-        break;
-
+    //Help
     case '?':
     case 'h':
         print_help();
         break;
 
+    //Bootloader
     case 'b':
         stock_reset_to_bootloader();
         break;
 
+    //WTF
     default:
         printf("Error: Unknown command 0x%02X (%c)\r\n", cmd_t[0], cmd_t[0]);
         break;
