c927e4c07ec8f44dce5fb773649922d700d6a6f7
John McMaster <johndmcmaster@gmail.com>
2018-11-05 21:29:53 -0800

    py: blinky example
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

---
+++ py/examples/blinky.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from otl866 import bitbang, util
+import time
+
+
+def run(port, verbose=False):
+    tl = bitbang.Bitbang(port, verbose=verbose)
+    while True:
+        tl.led(1)
+        time.sleep(0.5)
+        tl.led(0)
+        time.sleep(0.5)
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Blink the LED')
+    parser.add_argument(
+        '--port', default=util.default_port(), help='Device serial port')
+    parser.add_argument("--verbose", action="store_true")
+    args = parser.parse_args()
+
+    run(args.port, verbose=args.verbose)
+
+
+if __name__ == "__main__":
+    main()
