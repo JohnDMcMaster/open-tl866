a94bfb83be719eb17ce79b24eec23a2af71828a6
John McMaster <johndmcmaster@gmail.com>
2018-11-11 16:11:11 -0800

    command.py: bash low level cli interface
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

---
+++ py/examples/command.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from otl866 import aclient, util
+import sys
+
+
+def run(port, cmd, args, reply=True, check=True, verbose=False):
+    tl = aclient.AClient(port, verbose=verbose)
+    res = tl.cmd(cmd, *args, reply=reply, check=check)
+    res = res.replace('\r', '')
+    # Each command echo
+    res = res[res.find('\n') + 1:]
+    print(res.strip())
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Blink the LED')
+    parser.add_argument(
+        '--port', default=util.default_port(), help='Device serial port')
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("cmd")
+    parser.add_argument("args", nargs='*')
+    args = parser.parse_args()
+
+    run(args.port, args.cmd, args.args, verbose=args.verbose)
+
+
+if __name__ == "__main__":
+    main()
