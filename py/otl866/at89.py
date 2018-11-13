d7f910389b81f73957811df1ce862c44859a33ca
John McMaster <johndmcmaster@gmail.com>
2018-11-12 22:38:35 -0800

    at89 py: verbose sig str error, remove dead commands
    Signed-off-by: John McMaster <johndmcmaster@gmail.com>

--- py/otl866/at89.py
+++ py/otl866/at89.py
@@ -31,7 +31,7 @@ sig_i2s = {
 
 def sig_str(sig):
     sigi = (sig[0] << 16) | (sig[1] << 8) | sig[2]
-    return sig_i2s.get(sigi, "unknown")
+    return sig_i2s.get(sigi, "unknown (0x%06X)" % sigi)
 
 
 class AT89(aclient.AClient):
@@ -119,12 +119,3 @@ class AT89(aclient.AClient):
         """
         res = self.match_line(r"Result: (.*)", self.cmd('B')).group(1)
         return res == "blank"
-
-    """
-    def tests(self):
-        self.cmd('T')
-        assert 0, 'FIXME: parse out'
-    """
-
-    def reset_vdd(self):
-        self.cmd('v')
